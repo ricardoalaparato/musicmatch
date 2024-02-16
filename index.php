33fd54437c96eb8f082c39199a3216e32fc2dae5
richi <ricardoalaparato@gmail.com>
2024-02-16 19:53:47 +0100

    include bootstrap local via composer

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <meta charset="utf-8">  
         <base href="/musicmatch/">   
         <!-- <link href="img/favicon-32x32.png" rel="icon"> TODO-->  
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> <!-- BOOTSTRAP 5.3.2. -->
+        <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" > <!-- BOOTSTRAP 5.3.2. -->
             
 
     </head>
@@ -21,6 +21,6 @@
             
             <?php include 'componentes/footer/footer.php';		// Pie ?>  
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> <!-- script de bootstrap -->
+        <script src="C:\laragon\www\musicmatch\vendor\twbs\bootstrap\dist\js\bootstrap.bundle.min.js"> </script> <!-- script de bootstrap -->
     </body>
 </html>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0f15dcc6b32e793427de1964fc05ca3cfd265c35',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0f15dcc6b32e793427de1964fc05ca3cfd265c35',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -41,6 +41,21 @@
             'dev_requirement' => false,
             'provided' => array(
                 0 => '3.0.0',
+            ),
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.0.2',
+            'version' => '5.0.2.0',
+            'reference' => '688bce4fa695cc360a0d084e34f029b0c192b223',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.0.2',
             ),
         ),
     ),
